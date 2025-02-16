836dda234b4deda330756e687276e4f50250c461
OlexandrBolotov <boltik4a9@gmail.com>
2025-02-16 20:49:20 +0200

    1

---
+++ src/main.js
@@ -0,0 +1,35 @@
+import { fetchImages } from './js/pixabay-api';
+import { renderGallery } from './js/render-functions';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import './css/styles.css';
+
+const form = document.querySelector('#search-form');
+const loader = document.querySelector('#loader');
+const gallery = document.querySelector('.gallery');
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const query = event.target.elements.query.value.trim();
+
+  if (!query) {
+    iziToast.error({ title: 'Error', message: 'Please enter a search query!' });
+    return;
+  }
+
+  gallery.innerHTML = '';
+  loader.style.display = 'block';
+
+  try {
+    const data = await fetchImages(query);
+    if (data.hits.length === 0) {
+      iziToast.warning({ title: 'No Results', message: 'Sorry, no images found. Try again!' });
+    } else {
+      renderGallery(data.hits);
+    }
+  } catch (error) {
+    iziToast.error({ title: 'Error', message: 'Failed to fetch images. Try again later.' });
+  } finally {
+    loader.style.display = 'none';
+  }
+});
